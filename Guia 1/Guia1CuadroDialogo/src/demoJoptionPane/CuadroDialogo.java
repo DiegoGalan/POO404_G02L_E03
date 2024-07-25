5832447d9bb5fb0474d9374974f21e4a87b2769c
DiegoGalan <131511310+DiegoGalan@users.noreply.github.com>
2024-07-24 18:19:34 -0600

    Add files via upload

---
+++ Guia 1/Guia1CuadroDialogo/src/demoJoptionPane/CuadroDialogo.java
@@ -0,0 +1,9 @@
+package demoJoptionPane;
+import javax.swing.JOptionPane;
+
+public class CuadroDialogo {
+    public static  void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Bienvenido a Intellij IDEA IDE");
+    }
+}
