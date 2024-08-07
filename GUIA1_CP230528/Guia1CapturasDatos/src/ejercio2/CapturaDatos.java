c06c8a195eddc29f2216064d6ad2dd1184d6cb05
Jonathan <144579297+Jonathan1212322@users.noreply.github.com>
2024-08-07 15:49:25 -0300

    Add files via upload

---
+++ GUIA1_CP230528/Guia1CapturasDatos/src/ejercio2/CapturaDatos.java
@@ -0,0 +1,16 @@
+
+package ejercio2;
+import java.util.*;
+public class CapturaDatos {
+    public static void main(String[]argumento){
+        Scanner reader = new Scanner(System.in);
+        String nombre= "";
+        int  edad;
+        System.out.println("Bienvenido a Intellij IDEA IDE");
+        System.out.println("Ingrese su nombre");
+        nombre=reader.nextLine();
+        System.out.println("Ingrese su edad: ");
+        edad=reader.nextInt();
+        System.out.println("Usted se llama "+nombre+" y tiene "+edad+" años");
+    }
+}
---
+++ GUIA1_CP230528/Guia1CuadroDialogo/src/demoJoptionPane/CuadroDialogo.java
@@ -0,0 +1,7 @@
+package demoJoptionPane;
+import javax.swing.JOptionPane;
+public class CuadroDialogo {
+    public static void main(String[]args){
+        JOptionPane.showMessageDialog(null,"Bienevnido a Intellij IDEA IDE");
+    }
+}
