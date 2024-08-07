a0ae0407cbcf7cd8ee1d56fdbca0b8725a549701
Jonathan <144579297+Jonathan1212322@users.noreply.github.com>
2024-08-07 15:57:25 -0300

    Add files via upload

---
+++ guia2/Switch1.java
@@ -0,0 +1,14 @@
+package guia2;
+
+public class Switch1 {
+
+    public static void main(String[] args){
+
+        int a = 1;
+        int b = 1;
+        char op = '+'; //Puede cambiar el operador aritmético
+
+        System.out.print("El resultado es: ");
+
+    }
+}
