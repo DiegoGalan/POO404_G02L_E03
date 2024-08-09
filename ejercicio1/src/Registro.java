70dc9d9c34de3ccd58d0ce5c9f1a0102845b63e7
DiegoGalan <diegogalang23@gmail.com>
2024-08-09 13:05:10 -0600

    4.3

--- ejercicio1/src/Registro.java
+++ ejercicio1/src/Registro.java
@@ -1,12 +1,14 @@
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 import java.util.*;
 public class Registro {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+
         String nombre= "";
         String apellido= "";
         String sexo = "";
+        String fechaNacimiento = "";
 
 
         //Solicitar el nombre
@@ -21,12 +23,17 @@ public class Registro {
         System.out.print("Ingrese su sexo");
         sexo = reader.nextLine();
 
+        //Fecha de nacimiento
+        System.out.print("Ingrese su fecha de nacimiento dd-mm-yyyy");
+        fechaNacimiento = reader.nextLine();
+
 
 
         //resultado de imprimir
         System.out.println("Tu nombre es " + nombre);
         System.out.println("Tu apellido es " + apellido);
         System.out.println("Tu sexo es " + sexo);
+        System.out.println("Tu fecha de nacimiento es " + fechaNacimiento);
 
 
     }
