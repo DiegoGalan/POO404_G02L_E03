bb68f2415accb31fe374f8f5e40b20971dafd43b
DiegoGalan <diegogalang23@gmail.com>
2024-08-09 12:22:03 -0600

    4.2

--- ejercicio1/src/Registro.java
+++ ejercicio1/src/Registro.java
@@ -4,11 +4,10 @@ import java.util.*;
 public class Registro {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        JOptionPane.showMessageDialog(null, "Hola Bienvenido al Ejercicio1");
         String nombre= "";
         String apellido= "";
         String sexo = "";
-        int edad;
+
 
         //Solicitar el nombre
         System.out.print("Ingrese su nombre");
@@ -22,27 +21,11 @@ public class Registro {
         System.out.print("Ingrese su sexo");
         sexo = reader.nextLine();
 
-        //Se ingresa la edad
-        System.out.print("Ingrese su edad ");
-        edad = reader.nextInt();
 
-        if (edad < 30){
-            System.out.print("Hola amigo " + nombre + apellido);
-        }
-
-        else if (edad > 30){
-            System.out.print("Buenos dias mr " + nombre + apellido);
-        }
-
-        if (edad < 0){
-            System.out.println("  Su edad no debe ser negativa");
-            return;
-        }
 
         //resultado de imprimir
         System.out.println("Tu nombre es " + nombre);
         System.out.println("Tu apellido es " + apellido);
-        System.out.println("Tu edad es " + edad);
         System.out.println("Tu sexo es " + sexo);
 
 
