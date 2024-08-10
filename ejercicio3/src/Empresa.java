a02558b9435b12c6799342443288af288fca3d84
Jonathan <144579297+Jonathan1212322@users.noreply.github.com>
2024-08-10 00:54:09 -0300

    Update Empresa.java

--- ejercicio3/src/Empresa.java
+++ ejercicio3/src/Empresa.java
@@ -1,3 +1,15 @@
 public class Empresa {
+public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan Diego Peña", 3000.00);
+        Gerente gerente1 = new Gerente("Diego Alberto Canizalez", 5000.00, 1500.00);
 
+        System.out.println("Información del Empleado:");
+        empleado1.mostrarInfo();
+        System.out.println("Salario: " + empleado1.calcularSalario());
+        System.out.println();
+
+        System.out.println("Información del Gerente:");
+        gerente1.mostrarInfo();
+        System.out.println("Salario Total: " + gerente1.calcularSalario());
+    }
 }
